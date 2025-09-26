e43c1c7c0cb8364ea5699bc8efd42a9af0fdfc37
Rasec <rasec.cordero05@gmail.com>
2025-09-26 13:37:52 -0400

    Arreglos dockerfile

--- OMSA/core/settings.py
+++ OMSA/core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['onrender.com',"localhost", "127.0.0.1", "192.168.2.6"]  # tu IP LAN real
+ALLOWED_HOSTS = ['.onrender.com',"localhost", "127.0.0.1", "192.168.2.6"]  # tu IP LAN real
 CSRF_TRUSTED_ORIGINS = ["http://192.168.2.6:8000"]
 
 
